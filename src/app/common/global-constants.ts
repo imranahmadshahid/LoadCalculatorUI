e796672330076dc96fc0c6018459cbe53aff6c78
Imran Ahmad Shahid <imran.ahmad.shahid@qoco.fi>
2022-10-14 03:48:21 +0300

    change api url

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,3 +1,3 @@
 export class GlobalConstants {
-    public static apiURL: string = "http://imranahmadshahid-001-site1.ctempurl.com/";
+    public static apiURL: string = "https://cors-anywhere.herokuapp.com/http://imranahmadshahid-001-site1.ctempurl.com/";
 }
